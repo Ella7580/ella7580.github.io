5854f84224cd473ee47c5e0e6c8e99d7d614e814
Ella7580 <54272401+Ella7580@users.noreply.github.com>
2019-08-23 00:46:47 +0000

    Update config.js

--- gexf-js-master/config.js
+++ gexf-js-master/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "16-32-2.gexf",
+    graphFile : "16-32.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
